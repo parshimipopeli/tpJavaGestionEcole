381a8a3c5f742d9ba0eb37b951f2127466174352
parshimipopeli <philippe.lamorskipro@gmail.com>
2021-02-10 13:15:29 +0100

    first commit

---
+++ src/metier/Eleve.java
@@ -0,0 +1,154 @@
+package metier;
+
+import java.time.LocalDate;
+
+import java.time.LocalDate;
+
+public class Eleve {
+    private static int compteur ;
+    int id ;
+    String nom ;
+    String prenom ;
+    LocalDate dateNaissance ;
+    int age;
+    Adresse adresse;
+
+
+    public Eleve(String nom, String prenom, LocalDate dateNaissance, int age, Adresse adresse) {
+        super();
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.age = age;
+        this.adresse = adresse;
+        this.id = compteur++ ;
+    }
+
+
+
+
+
+    public Eleve() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+
+
+
+    public Eleve(int id, String nom, String prenom, LocalDate dateNaissance, int age, Adresse adresse) {
+        super();
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.age = age;
+        this.adresse = adresse;
+    }
+
+
+
+
+    public static int getCompteur() {
+        return compteur;
+    }
+    public static void setCompteur(int compteur) {
+        Eleve.compteur = compteur;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public LocalDate getDateNaissance() {
+        return dateNaissance;
+    }
+    public void setDateNaissance(LocalDate dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public Adresse getAdresse() {
+        return adresse;
+    }
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+
+
+
+
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + age;
+        result = prime * result + ((dateNaissance == null) ? 0 : dateNaissance.hashCode());
+        result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+        result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Eleve other = (Eleve) obj;
+        if (age != other.age)
+            return false;
+        if (dateNaissance == null) {
+            if (other.dateNaissance != null)
+                return false;
+        } else if (!dateNaissance.equals(other.dateNaissance))
+            return false;
+        if (nom == null) {
+            if (other.nom != null)
+                return false;
+        } else if (!nom.equals(other.nom))
+            return false;
+        if (prenom == null) {
+            if (other.prenom != null)
+                return false;
+        } else if (!prenom.equals(other.prenom))
+            return false;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Eleve [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", dateNaissance=" + dateNaissance + ", age="
+                + age + ", adresse=" + adresse.toString() + "]\n";
+    }
+
+
+
+}
+
+
---
+++ src/metier/Salle.java
@@ -0,0 +1,104 @@
+package metier;
+
+public class Salle {
+
+    public	static int compteur = 0;
+    int id ;
+    String code ;
+    String libelle;
+
+
+    public Salle() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+    public Salle(String code, String libelle) {
+        this.code = code;
+        this.libelle = libelle;
+        this.id = compteur++ ;
+    }
+
+
+
+
+    public Salle(int id, String code, String libelle) {
+        super();
+        this.id = id;
+        this.code = code;
+        this.libelle = libelle;
+    }
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+
+    @Override
+    public String toString() {
+        return " \n Salle [id=" + id + ", code=" + code + ", libelle=" + libelle + "]";
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((code == null) ? 0 : code.hashCode());
+        result = prime * result + ((libelle == null) ? 0 : libelle.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Salle other = (Salle) obj;
+        if (code == null) {
+            if (other.code != null)
+                return false;
+        } else if (!code.equals(other.code))
+            return false;
+        if (libelle == null) {
+            if (other.libelle != null)
+                return false;
+        } else if (!libelle.equals(other.libelle))
+            return false;
+        return true;
+    }
+
+
+
+
+
+}
